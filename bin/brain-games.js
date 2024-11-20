3e2f2bdd98ca84e307aaaee4d4976b31208c0302
Daniel Miquilena <dmiquilena95@LENOVO-DM>
2024-11-20 17:07:15 -0500

    modified import commands

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import greetUser from '/home/dmiquilena95/frontend-project-98/cli.js';
+import greetUser from '../cli.js';
 import readlineSync from 'readline-sync';
 
 greetUser();
